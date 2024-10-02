d9156c43596e9a24383c5139e456d6bb8690fc11
Jay Baker <logikal@gmail.com>
2024-10-03 08:55:50 +1300

    add tests for registering using tags

---
+++ tests/decorators/inject-tag.test.ts
@@ -0,0 +1,36 @@
+import { it, describe } from "node:test"
+import assert from "node:assert/strict"
+import { inject } from "../../src/decorators/inject"
+import { injectable } from "../../src/decorators/injectable"
+import { container } from "../../src"
+
+abstract class ILogger {
+  abstract info(...args: any[]): void
+}
+
+@injectable
+class ConsoleLogger {
+  info(...args: any[]) {
+    console.log(...args)
+  }
+}
+
+@injectable
+class NullLogger {
+  info(...args: any[]) {
+    return void 0
+  }
+}
+
+describe('inject with tags', () => {
+  it('should resolve the correct dependency with a tag', () => {
+    container.register(ILogger, { useClass: ConsoleLogger, tag: "console" })
+    container.register(ILogger, { useClass: NullLogger, tag: "null" })
+    
+    const nullLogger = container.resolve(ILogger, "null")
+    assert.equal(nullLogger instanceof NullLogger, true)
+
+    const consoleLogger = container.resolve(ILogger, "console")
+    assert.equal(consoleLogger instanceof ConsoleLogger, true)
+  })
+})
