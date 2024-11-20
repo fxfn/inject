595a90cec1f562f3183587b82102aa8d4982715b
Jay Baker <logikal@gmail.com>
2024-11-21 07:34:46 +1300

    fix: allow the @inject decorator to pass a tag through if necessary

--- src/decorators/inject.ts
+++ src/decorators/inject.ts
@@ -1,13 +1,13 @@
 import { container } from "../container"
 import { InjectionToken } from "../types"
 
-export function inject(token: InjectionToken<any>) {
+export function inject(token: InjectionToken<any>, tag?: string) {
   return function (
     target: undefined,
     context: ClassFieldDecoratorContext<any, any>
   ) {
     return function (value: any): any {
-      return container.resolve(token)
+      return container.resolve(token, tag)
     }
   }
 }
